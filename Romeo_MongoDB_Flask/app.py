320200bea3442ee8801810b84d652cea1a2a748c
Gaetano <147176705+GaetanoRomeo22@users.noreply.github.com>
2025-04-24 22:36:26 +0200

    Add files via upload

---
+++ Romeo_MongoDB_Flask/app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from pymongo import MongoClient
+import bcrypt
+
+
+app = Flask(__name__)
+app.secret_key = 'Gaetano22'
+
+try:  # Tentativo di connessione al database MongoDB
+    username = 'Gaetano22'
+    password = 'Vittoria_30'
+    client = MongoClient(
+        f'mongodb+srv://{username}:{password}@segreteria.nzypczo.mongodb.net/?retryWrites=true&w=majority&appName=Segreteria')
+    db = client['Segreteria']
+except Exception as e:
+    print(f"Errore connessione database: {e}")
+
+
+@app.route('/')  # Route principale
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index')  # Route per la home page
+def back_home():
+    return render_template('index.html')
+
+
+@app.route('/showRegister')  # Route per la pagina di registrazione
+def show_register():
+    return render_template('registrazione.html')
+
+
+@app.route('/showRetrievePassword')  # Route per la pagina di recupero password
+def show_retrieve_password():
+    return render_template('recupero_password.html')
+
+
+@app.route('/showAccount')  # Route per la pagina dell'account
+def show_account():
+    return render_template('account.html')
+
+
+@app.route('/showAvailableExams') # Route per la pagina degli esami prenotabili
+def show_available_exams():
+    return render_template('appelli_disponibili.html')
+
+
+@app.route('/showGivenExams') # Route per la pagina del libretto degli esami
+def show_given_exams():
+    return render_template('libretto.html')
+
+
+@app.route('/showBookedExams') # Route per la pagina degli esami prenotati
+def show_booked_exams():
+    return render_template('appelli_prenotati.html')
+
+
+@app.route('/login', methods=['POST'])  # Route per il login
+def login():  # Funzione di login
+    matricola = request.form['matricola']  # Recupera la matricola dal form
+    password = request.form['password']  # Recupera la password dal form
+    if not matricola or not password:  # Controlla se matricola e password sono vuoti
+        flash('Matricola o password mancanti')
+        return redirect(url_for('index'))
+    else:
+        try:
+            user = db.users.find_one({'matricola': matricola, 'password': password})  # Cerca l'utente nel database
+            if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):  # Se l'utente esiste
+                return redirect(url_for('show_account'))  # Reindirizza alla pagina dell'account
+            else:  # Se l'utente non esiste
+                flash('Matricola o password errati')
+                return redirect(url_for('index'))  # Resta alla pagina di login
+        except Exception as exc:
+            flash("Errore durante il login: " + str(exc))
+            return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['POST'])  # Route per la registrazione
+def register():  # Funzione di registrazione
+    matricola = request.form['matricola']  # Recupera la matricola dal form
+    password = request.form['password']  # Recupera la password dal form
+    if not matricola or not password:  # Controlla se matricola e password sono vuoti
+        flash('Matricola o password mancanti')
+        return redirect(url_for('show_register'))
+    else:
+        try:
+            user = db.users.find_one({'matricola': matricola})  # Cerca l'utente nel database
+            if user:  # Se l'utente viene trovato significa che la matricola è già utilizzata
+                flash('Matricola già utilizzata')
+                return redirect(url_for('show_register'))  # Resta alla pagina di registrazione
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())  # Hashing della password
+            db.users.insert_one({'matricola': matricola, 'password': hashed_password})  # Inserisce l'utente nel database
+            return redirect(url_for('index'))  # Reindirizza alla pagina di login
+        except Exception as exc:
+            flash("Errore durante la registrazione: " + str(exc))
+            return redirect(url_for('show_register'))
+
+
+@app.route('/logout') # Route per il logout
+def logout(): # Funzione di logout
+    return redirect(url_for('index')) # Reindirizza alla pagina di login
+
+
+if __name__ == '__main__':
+    app.run()
